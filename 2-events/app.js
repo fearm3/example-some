41bea73101e1d5e5d8ef429c828a122f8da2da9b
fearm3 <ibraaa1628@gmail.com>
2022-01-10 01:27:47 +0300

    add event

--- 2-events/app.js
+++ 2-events/app.js
@@ -1,37 +1,13 @@
 const app = Vue.createApp({
   data() {
     return {
-      title: "VueJs start",
-      content: "lorem ipsum",
-      eduflow: {
-        title: "VeuJs Examples",
-        target: "_blank",
-        url: "https://v3.vuejs.org/",
-        alt: "Awesome Vue.js",
-      },
-      owner: "from App to Some thing else",
-      coords: {
-        x: 0,
-        y: 0,
-      },
+      fullname: "Ahmet",
     };
   },
   methods: {
-    changeTitle(pTitle) {
-      //   alert();
-      //   this.title = "This will be changed!";
-      this.title = pTitle;
-    },
-    updateCoords(message, event) {
-      //   console.log(event.x, event.y);
-      //   console.log(message, event.x, event.y);
-      //   this.coords.x = event.x;
-      //   this.coords.y = event.y;
-      this.changeTitle(`${event.x}, ${event.y}`);
-      this.coords = {
-        x: event.x,
-        y: event.y,
-      };
+    updateValue(event) {
+      console.log(event.target.value);
+      this.fullname = event.target.value;
     },
   },
 }).mount("#app");
