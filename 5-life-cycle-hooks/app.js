a7021ce26763b7d2aa42a001b2891d74aea6ff18
fearm3 <ibraaa1628@gmail.com>
2022-01-10 23:58:37 +0300

    add lifecyling

--- 5-life-cycle-hooks/app.js
+++ 5-life-cycle-hooks/app.js
@@ -2,7 +2,33 @@ const app = Vue.createApp({
   data() {
     return {
       title: "Test title",
+      itemList: [],
     };
+  },
+  beforeCreate() {
+    console.log("beforeCreate working...");
+  },
+  created() {
+    console.log("created working...");
+    setTimeout(() => {
+      this.itemList = [1, 2, 3, 4, 5, 6, 7, 9];
+    }, 2000);
+  },
+  beforeMount() {
+    console.log("beforeMount working...");
+  },
+  mounted() {
+    console.log("mounted working...");
+  },
+  beforeUpdate() {
+    console.log("beforeUpdate working...");
+  },
+  updated() {
+    console.log("updated working...");
   },
 });
 app.mount("#app");
+
+setTimeout(() => {
+  app.unmount();
+}, 5000);
