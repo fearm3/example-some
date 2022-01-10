db1e06ef7d2d68762a41f7355cc57a102a4e2f68
fearm3 <ibraaa1628@gmail.com>
2022-01-10 23:48:15 +0300

    add commit

--- 4-reactivity/app.js
+++ 4-reactivity/app.js
@@ -14,7 +14,8 @@ const app = Vue.createApp({
   },
   computed: {
     filteredList() {
-        return this.itemList.filter((i) => i.includes(this.search));
+      return this.itemList.filter((i) => i.includes(this.search));
     },
   },
-}).mount("#app");
+});
+app.mount("#app");
